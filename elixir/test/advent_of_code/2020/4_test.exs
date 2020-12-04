cf420619b08614cb6e082b75e172bc2a31955222
Solomon White <solomon@blinker.com>
2020-12-03 23:01:51 -0700

    elixir day 4

---
+++ elixir/test/advent_of_code/2020/4_test.exs
@@ -0,0 +1,35 @@
+defmodule AdventOfCode.Y2020.D4Test do
+  use ExUnit.Case
+
+  alias AdventOfCode.Y2020.D4
+
+  describe "solution1" do
+    test "works for sample input" do
+      assert D4.solution1(D4.sample_input) == 2
+    end
+
+    test "works for real input" do
+      assert D4.solution1(D4.real_input) == 196
+    end
+  end
+
+  describe "solution2" do
+    test "valid passports are valid" do
+      D4.valid_passport_input()
+      |> String.split("\n\n", trim: true)
+      |> Enum.map(&D4.parse_passport/1)
+      |> Enum.each(fn p -> assert D4.valid?(p) end)
+    end
+
+    test "invalid passports are invalid" do
+      D4.invalid_passport_input()
+      |> String.split("\n\n", trim: true)
+      |> Enum.map(&D4.parse_passport/1)
+      |> Enum.each(fn p -> refute D4.valid?(p) end)
+    end
+
+    test "works for real input" do
+      assert D4.solution2(D4.real_input) == 115
+    end
+  end
+end
